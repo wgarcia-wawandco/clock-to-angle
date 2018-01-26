e8cf8f601cfd472cfbf6eb36f671c31e139464ac
Walderman Garcia <wgarcia@wawand.co>
2018-01-26 14:12:01 -0500

    Javascript ES6

--- js/script.js
+++ js/script.js
@@ -13,22 +13,59 @@ function validate(hour, min){
   return false;
 }
 
-const hourToAngle = hrs=> (hrs * 360) / 12;
-const hourDesv = mnts => (mnts * 30) / 60;
+const hourToAngle = hrs => (hrs * 360) / 12;
+const minToAngle = min =>(min * 360) / 60;
+const hourDesv = min => (min * 30) / 60;
+
+function showResult(hour, min, result){
+  let error = document.getElementById('error');
+  let hResult = document.getElementById('hourResult');
+  let mResult = document.getElementById('minResult');
+  let infoMsj = document.getElementById('info-message');
+  let resultMsj = document.getElementById('result-message');
+  let res = document.getElementById('final-value');
+  if(hour == 0){
+    hour = 12;
+  }
+  let txtHour = `${hour} Hours`,
+      txtMin = `${min} Minutes`,
+      txtResult = result;
+  if(hour == 1){
+    txtHour = `${hour} Hour`;
+  }
+  if(min == 1){
+    txtMin = `${min} Minute`;
+  }
+  hResult.textContent = txtHour;
+  mResult.textContent = txtMin;
+  error.classList.remove('visible');
+  infoMsj.classList.remove('visible');
+  resultMsj.classList.add('visible');
+  res.textContent = result;
+}
+
+function showError(){
+  document.getElementById('error').classList.add('visible');
+  document.getElementById('info-message').textContent = 'Write a valid value.';
+  document.getElementById('info-message').classList.add('visible');
+  document.getElementById('result-message').classList.remove('visible');
+  document.getElementById('final-value').textContent = 0;
+}
 
 function calcAngle(){
   let [hour, min] = getData();
   console.log(hour);
   console.log(min);
   if (validate(hour, min)){
-    let txtHour
     if(hour == 12){
       hour = 0;
-      txtHour == '12 Hours';
     }
-
-    let hour_angle = hourToAngle(hour),
-        min_angle = hourToAngle(min/60),
-        desv = hourDesv(min);
+    let hourAngle = hourToAngle(hour),
+        minAngle = minToAngle(min),
+        desvAngle = hourDesv(min);
+    let result  = Math.abs(hourAngle + desvAngle - minAngle);
+    showResult(hour, min, result);
+  }else{
+    showError();
   }
 }
